88ca191a3f77f44931ae3dba6872c438fd9878d6
Nathan Orlowski <norlowski@gmail.com>
2021-04-14 11:56:04 -0400

    bearer token

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let api = axios.create({
     timeout: 10000,
     headers: ({
-        'Authorization': 'Bearer qjutzqmo0amynhp2uxz9r4g7g7aabh',
+        'Authorization': 'Bearer 6jscr2xdvsgd1ceakk92g7e7zaixav',
         'Client-ID': 'o9l59asu29au6foaq5zoeo91xz29bo'
     })
 });

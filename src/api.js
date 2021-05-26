198795bc5d6b20fc91193408d8b43953a33e8509
Nathan Orlowski <norlowski@gmail.com>
2021-05-26 15:22:05 -0400

    Updates May 26

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let api = axios.create({
     timeout: 10000,
     headers: ({
-        'Authorization': 'Bearer 6jscr2xdvsgd1ceakk92g7e7zaixav',
+        'Authorization': 'Bearer kf32t6dhhh2p1ubly9e8bkzqb3lfvc',
         'Client-ID': 'o9l59asu29au6foaq5zoeo91xz29bo'
     })
 });

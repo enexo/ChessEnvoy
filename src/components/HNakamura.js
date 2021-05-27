8310ff21376fa07ac4bdd94f5c21748aa6ea8fdb
Nathan Orlowski <norlowski@gmail.com>
2021-05-27 14:31:49 -0400

    iframe parent changed hikaru

--- src/components/HNakamura.js
+++ src/components/HNakamura.js
@@ -7,7 +7,7 @@ const HNakamura = () => (
         <div className="container">
             <div className="row">
                 <div className="col">
-                    <iframe src="https://player.twitch.tv/?channel=gmhikaru&parent=localhost" frameBorder="0"
+                    <iframe src="https://player.twitch.tv/?channel=gmhikaru&parent=chessenovy.netlify.app" frameBorder="0"
                             allowFullScreen="true" scrolling="no" height="378" width="620" title="Hikaru Plays Chess">
                     </iframe>
                 </div>
